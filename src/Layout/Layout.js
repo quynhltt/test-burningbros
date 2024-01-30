824ba0ce7258af782c9a09db182f34afcdc01b50
Quynh Le <quynh.le2199@gmail.com>
2024-01-30 16:12:24 +0700

    remove unrelevant file

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -7,7 +7,6 @@ export default function Layout({ Component }) {
     <div className='space-y-10 h-full min-h-screen flex flex-col'>
       <Header />
       <div className='flex-grow pt-20'><Component /></div>
-      {/* <Footer/> */}
     </div>
   )
 }
